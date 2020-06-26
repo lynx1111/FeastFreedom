30a552bfe2ee817f104e5bca897df6078a44e732
lynx1111 <qunchaozhou@gmail.com>
2020-06-25 20:47:14 -0700

    update angular

--- FeastFreedom-angular/karma.conf.js
+++ FeastFreedom-angular/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/FeastFreedom'),
+      dir: require('path').join(__dirname, './coverage/FeastFreedom-angular'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
--- FeastFreedom-angular/src/app/app.component.spec.ts
+++ FeastFreedom-angular/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'FeastFreedom'`, () => {
+  it(`should have as title 'FeastFreedom-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('FeastFreedom');
+    expect(app.title).toEqual('FeastFreedom-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('FeastFreedom app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('FeastFreedom-angular app is running!');
   });
 });
